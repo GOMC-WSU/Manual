4616c8117993f92362cf6dc1bc09b272a11eec95
GregorySchwing <gschwing@uno.edu>
2022-07-05 10:59:33 -0500

    revert readme and compile

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
 rm -r docs
 sphinx-build src docs
 cd docs
@@ -8,9 +7,3 @@ mv _static static
 grep -rli '_images' * | xargs -n 1 sed -i '' -e "s/_images/images/g"
 grep -rli '_sources' * | xargs -n 1 sed -i '' -e "s/_sources/sources/g"
 grep -rli '_static' * | xargs -n 1 sed -i '' -e "s/_static/static/g"
-# To build PDF 
-cd ..
-sphinx-build  -b latex  src  build
-cd build
-make
-mv GOMC.pdf ../GOMC_Manual_2.75.pdf
